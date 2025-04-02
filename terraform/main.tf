d2d98187eb453861ea9d881aaa371960c50f2869
mikhail.barsukou <mikhail.barsukou@lottoland.com>
2025-04-02 18:49:36 +0200

    initial commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,7 @@
 provider "google" {
-  project = var.project_id
-  region  = var.region
+  project     = var.project_id
+  region      = var.region
+  credentials = file("/tmp/gcp-key.json")
 }
 
 # Enable Firebase Services
