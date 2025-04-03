b9a8b15bcb071ef3b4978bea66adcd870a727ea9
mikhail.barsukou <mikhail.barsukou@lottoland.com>
2025-04-03 14:25:16 +0200

    initial commit

--- terraform/check_firestore.sh
+++ terraform/check_firestore.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 DOCUMENT_NAME="projects/poc-car-delivery/databases/(default)/documents/settings/adminList"
-
+echo 'HELLLOOOOO'
 if gcloud firestore documents describe "$DOCUMENT_NAME" --format="json" > /dev/null 2>&1; then
     echo '{"exists": "true"}'
 else
