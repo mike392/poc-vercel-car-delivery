3a0c62badfd9e5495aa7c7e754a5d217d620c24c
mikhail.barsukou <mikhail.barsukou@lottoland.com>
2025-04-03 15:21:28 +0200

    initial commit

--- car-tracker/next.config.mjs
+++ car-tracker/next.config.mjs
@@ -4,6 +4,7 @@ const { NextConfig } = pkg
 /** @type {NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    output: "export",
     images: {
         domains: ["firebasestorage.googleapis.com"], // Allow images from Firebase Storage
     },
