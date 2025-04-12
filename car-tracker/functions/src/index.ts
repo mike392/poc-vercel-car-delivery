6239374d67cf27e4a68468bcf2d54fa95310c350
mikhail.barsukou <mikhail.barsukou@lottoland.com>
2025-04-12 14:47:29 +0200

    fixing server component

--- car-tracker/functions/src/index.ts
+++ car-tracker/functions/src/index.ts
@@ -1,24 +1,23 @@
-import path from "node:path";
+import { onRequest } from 'firebase-functions/v2/https';
+import * as path from 'path';
+import { Request, Response } from 'express';
+import next from 'next';
+import * as admin from 'firebase-admin';
 
-const functions = require('firebase-functions');
-const next = require('next');
-// import { NextApiRequest, NextApiResponse } from "next";
-// import next from "next";
-import { Request, Response } from "express";
-
-// Initialize Firebase Admin SDK
-const admin = require('firebase-admin');
 admin.initializeApp();
 
 const nextApp = next({
-    dev: false,  // Change to `true` for development
-    dir: path.join(__dirname, '../'),   // The directory of your Next.js app
+    dev: false,
+    dir: path.join(__dirname, '../'),
 });
-
 const handle = nextApp.getRequestHandler();
 
-exports.nextjs = functions.region('europe-west1').https.onRequest((req: Request, res: Response) => {
-    return nextApp.prepare().then(() => {
-        handle(req, res);
-    });
-});
+export const nextjs = onRequest(
+    {
+        region: 'europe-west1',
+        // You can also specify memory, concurrency, timeout seconds, etc.
+    },
+    (req: Request, res: Response) => {
+        return nextApp.prepare().then(() => handle(req, res));
+    }
+);
